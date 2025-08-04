3b85a1edb36c9df88e165dfb1a7f529f7967738c
Turok <illia.turok@student.tuke.sk>
2025-08-04 14:07:43 +0200

    Fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
-import { getAllTodos } from "../lib/api";
-import AddTask from "./components/AddTask";
-import TodoList from "./components/TodoList";
+// import { getAllTodos } from "../lib/api";
+// import AddTask from "./components/AddTask";
+// import TodoList from "./components/TodoList";
 
-export default async function Home() {
-  const tasks = await getAllTodos();
-  console.log(tasks);
+// export default async function Home() {
+//   const tasks = await getAllTodos();
+//   console.log(tasks);
 
+//   return (
+//     <main className="max-w-4xl mx-auto mt-4">
+//       <div className="text-center my-5 flex flex-col gap-4">
+//         <h1 className="text-2xl font-bold">Todo List</h1>
+//         <AddTask />
+//       </div>
+//       <TodoList tasks={tasks} />
+//     </main>
+//   );
+// }
+export default function Home() {
   return (
-    <main className="max-w-4xl mx-auto mt-4">
-      <div className="text-center my-5 flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Todo List</h1>
-        <AddTask />
-      </div>
-      <TodoList tasks={tasks} />
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Hello World 🌍</h1>
     </main>
   );
 }
