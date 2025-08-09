2877f526f2a3caeea9a7f92e65055f535cccfcbd
Turok <illia.turok@student.tuke.sk>
2025-08-09 18:42:02 +0200

    Some changes for vercel

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+// "Клиентский" helper: импортируй только из клиентских компонентов
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
